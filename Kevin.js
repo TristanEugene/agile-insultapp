5e8bf5cfe2bbda3dd1579ee22733ccdfdb58bb5e
KevinDom-dev <kvndom@gmail.com>
2019-09-17 11:30:13 +0200

    testing proxy

---
+++ Kevin.js
@@ -0,0 +1,23 @@
+/*var request = new XMLHttpRequest();
+
+request.open('GET', 'https://insultgenerator.apiblueprint.org/insults');
+console.log(request);
+request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+    }
+};
+
+request.send();
+*/
+window.onload=function() {
+    const proxy = "https://git.heroku.com/safe-lowlands-88473.git";
+    const url = "https://insult.mattbas.org/api/insult";
+    fetch(proxy + url)
+    .then(response => response.json())
+    .then(function (data) {
+            console.log(data)
+    });
+};
