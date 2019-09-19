0c28d65e17034f1b8d23f59159436d0980474263
KevinDom-dev <kvndom@gmail.com>
2019-09-19 10:42:16 +0200

    working version

--- assets/widgets.js
+++ assets/widgets.js
@@ -1,4 +1,4 @@
-window.twttr = (function(d, s, id) {
+window.twttr = (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
         t = window.twttr || {};
     if (d.getElementById(id)) return t;
@@ -8,9 +8,17 @@ window.twttr = (function(d, s, id) {
     fjs.parentNode.insertBefore(js, fjs);
 
     t._e = [];
-    t.ready = function(f) {
+    t.ready = function (f) {
         t._e.push(f);
     };
 
     return t;
 }(document, "script", "twitter-wjs"));
+
+$(document).ready(function () {
+    let content = document.getElementById("insult").innerHTML;
+    let link = document.getElementById("share");
+    twttr.widgets.load(link.setAttribute("data-text", "'" + content + "'"));
+
+});
+
