b4b6944831b0db77f7a36233c127c4788cc085d4
KevinDom-dev <kvndom@gmail.com>
2019-09-20 09:58:28 +0200

    new api

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-fetch("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "lakerolmaker-insult-generator-v1.p.rapidapi.com",
-        "x-rapidapi-key": "b1dcdd3f4emshdf12f6eaacebbd3p1e8e38jsnd8b135cf0b2b"
-    }
-})
+fetch("https://api-gateway-becode.herokuapp.com/?goto=https://insult.mattbas.org/api/insult")
     .then(function (response) {
         return response.text();
 
     })
     .then(function (data) {
+        console.log(data);
         document.getElementById("insult").innerText = data;
 
     });
